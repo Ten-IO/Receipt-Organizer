2e8322a04445cd758d7c377d6cdbf5cc91cf4aab
Ten_IO <poktepouten.info@protonmail.com>
2025-03-02 11:56:10 +0700

    remove init app

--- App.java
+++ App.java
@@ -1,5 +1,22 @@
+import recipe.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Recipes recipe = new Recipes();
+        recipe.makeReceipt("Cheese cake", "flour, cheese, sugar, salt", "Western",
+                "Heat oven, put flour and other ingredient, cook");
+        recipe.makeReceipt("Chocolate cake", "flour, cheese, sugar, salt", "Western",
+                "Heat oven, put flour and other ingredient, cook");
+        recipe.makeReceipt("Chocolate cake", "flour, cheese, sugar, salt", "Western",
+                "Heat oven, put flour and other ingredient, cook");
+        recipe.makeReceipt("Roman cake", "flour, cheese, sugar, salt", "Western",
+                "Heat oven, put flour and other ingredient, cook");
+        recipe.makeReceipt("Aspirant cake", "flour, cheese, sugar, salt", "Western",
+                "Heat oven, put flour and other ingredient, cook");
+
+        System.out.println("recipe added");
+        System.out.println("Current Recipes number: " + recipe.getCount());
+        recipe.writeToTxt();
+        
     }
 }
