54bbb5b248e46e0645210f1f99f6a8933c346e20
Meas Ratanakviphou <83325447+Skynoxk@users.noreply.github.com>
2025-03-21 14:14:52 +0700

    Update ChatBot.java

--- frontDesign/ChatBot.java
+++ frontDesign/ChatBot.java
@@ -45,14 +45,8 @@ public class ChatBot {
     @Override
     public void actionPerformed(ActionEvent e) {
         String userInput = inputField.getText().trim();
-        
-        if (userInput.equals("How many food in my application")) {
-            chatArea.append("How many food in my application by crawling this https://github.com/Ten-IO/Receipt-Organizer/blob/main/src/files/localFood.txt\n");
-            String response = getGeminiResponse(userInput);
-            chatArea.append("Gemini: " + response + "\n\n");
-            inputField.setText("");
-        } else if (!userInput.isEmpty()) {
-            chatArea.append("You: " + userInput + "\n");
+       if (!userInput.isEmpty()) {
+            chatArea.append("You: " + "by crawling this https://github.com/Ten-IO/Receipt-Organizer/blob/main/src/files/localFood.txt" + userInput + "\n");
             String response = getGeminiResponse(userInput);
             chatArea.append("Gemini: " + response + "\n\n");
             inputField.setText("");
